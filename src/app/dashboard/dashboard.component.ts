1bca783f57cb27fda157ce4893eb35dfe891b268
Peter <peter.i.rangelov.ctr@mail.mil>
2025-01-02 16:00:02 -0500

    upgraded to angular v19

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-	selector: 'rt-dashboard',
-	standalone: true,
-	templateUrl: './dashboard.component.html',
-	styleUrls: ['./dashboard.component.scss'],
-	imports: [],
-	changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'rt-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.scss'],
+    imports: [],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DashboardComponent {
 
